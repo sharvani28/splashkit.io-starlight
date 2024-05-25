6044f21d9fcd106cb96de2663a80b4710c27d83f
sharvani28 <sharvanikandala@gmail.com>
2024-05-25 21:33:46 +1000

    Modified fill_triangle function
    Usage example of fill_triangle function with 4 files.

--- src/assets/usage-examples-code/graphics/fill_triangle/fill_triangle-2-basic-red-triangle.cs
+++ src/assets/usage-examples-code/graphics/fill_triangle/fill_triangle-2-basic-red-triangle.cs
@@ -4,7 +4,7 @@ using static SplashKitSDK.SplashKit;
 // Open a window titled "Fill Triangle Example" with dimensions 800x600
 OpenWindow("Fill Triangle Example", 800, 600);
 
-ClearScreen(ColorWhite());
+ClearScreen();
 
 // Fill a triangle with vertices (100, 100), (200, 200), and (300, 100) with red color
 FillTriangle(Color.Red, 100, 100, 200, 200, 300, 100);
